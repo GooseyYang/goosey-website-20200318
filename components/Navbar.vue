8309e23ef1a8dc923ae74211e582760ebebdc6a3
lisha.yang <lisha.yang@mihoyo.com>
2020-03-18 13:02:30 +0800

    Navbar

---
+++ components/Navbar.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="navbar__bg">
+    <div class="navbar__fg">
+      <div class="logo"></div>
+      <ul class="navbar__list">
+        <li class="navbar__list__li active">首页</li>
+        <li class="navbar__list__li">一本正经鹅</li>
+        <li class="navbar__list__li">五彩缤纷鹅</li>
+        <li class="navbar__list__li">关于鹅</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar'
+}
+</script>
+<style scoped>
+  .navbar__bg {
+    width: 100%;
+    height: 3.75rem;
+    border-top: .625rem solid #6a4835;
+    background-color: #eee;
+  }
+
+  .navbar__fg {
+    width: 80rem;
+    height: 100%;
+    margin: 0 auto;
+  }
+
+  .logo {
+    display: inline-block;
+    width: 3.75rem;
+    height: 3.75rem;
+    background: url('../assets/images/goose.jpg') no-repeat;
+    background-size: 100%;
+  }
+
+  .navbar__list {
+    float: right;
+  }
+
+  .navbar__list__li{
+    float: left;
+    width: 9.375rem;
+    height: 3.125rem;
+    line-height: 3.125rem;
+    vertical-align: middle;
+    text-align: center;
+  }
+
+  .navbar__list__li:hover,
+   .navbar__list .active {
+    background-color: #323232;
+    color: #ffffff;
+    cursor: pointer;
+  }
+
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,12 @@
 <template>
-  <div>
+  <div class="root">
     <nuxt />
   </div>
 </template>
 
 <style>
 html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-family: Microsoft YaHei, Arial, sans-serif;
   font-size: 16px;
   word-spacing: 1px;
   -ms-text-size-adjust: 100%;
@@ -22,6 +21,25 @@ html {
 *:after {
   box-sizing: border-box;
   margin: 0;
+}
+
+ul {
+  list-style: none;
+}
+
+.root {
+  width: 100%;
+  min-width: 1280px;
+}
+
+.clear-fix {
+  zoom: 1;
+}
+
+.clear-fix:after {
+  content: '';
+  display: block;
+  clear: both;
 }
 
 .button--green {
