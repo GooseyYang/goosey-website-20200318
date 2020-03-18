36187878bd3d41b487b6ea3dcf91c4bbf1e6321b
lisha.yang <lisha.yang@mihoyo.com>
2020-03-18 13:43:49 +0800

    carousel.vue

---
+++ components/carousel.vue
@@ -0,0 +1,30 @@
+<template>
+  <el-carousel class="carousel">
+    <el-carousel-item v-for="item in 1" :key="item">
+      <nuxt-link 
+        to="/"
+        class="small">{{ item }}</nuxt-link>
+    </el-carousel-item>
+  </el-carousel>
+</template>
+<script>
+export default {
+  name: 'Carousel'
+}
+</script>
+<style scoped>
+.carousel {
+  display: inline-block;
+  width: 38.125rem;
+  height: 16.875rem;
+}
+
+.el-carousel__item:nth-child(2n) {
+    background-color: #99a9bf;
+}
+
+.el-carousel__item:nth-child(2n+1) {
+    background-color: #d3dce6;
+}
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,31 @@
 <template>
-  <Navbar/>
-  
+  <div>
+    <navbar/>
+    <div class="main">
+      <div>
+        <carousel/>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 import Navbar from '../components/navbar'
+import Carousel from '../components/carousel'
 
 export default {
-  name: 'index',
+  name: 'Index',
   components: {
-    Navbar
+    Navbar,
+    Carousel
   }
 }
 </script>
 
 <style>
-
+.main {
+  width: 80rem;
+  height: aotu;
+  margin: 5rem auto;
+}
 </style>
